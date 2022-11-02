7e8a5f81c2f285c08f3418411f8175a05f10fd4f
shveppi <info@shveppi.ru>
2022-11-02 12:50:15 +0300

    правки

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -12,5 +12,6 @@
   @php
     $default_error_message = "Пожалуй <a href='javascript:history.back()''>вернусь назад</a> или <a href='".url('')."'>на главную</a>.";
   @endphp
-  {!! isset($exception)? ($exception->getMessage()?$exception->getMessage():$default_error_message): $default_error_message !!}
+  {{-- isset($exception)? ($exception->getMessage()?$exception->getMessage():$default_error_message): $default_error_message --}}
+  {!! $default_error_message !!}
 @endsection
